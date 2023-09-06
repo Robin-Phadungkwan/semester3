3b672286581880654e1afab488bd57afd6d490da
Robin Phadungkwan <robin.phadungkwan@hva.nl>
2023-09-06 14:20:09 +0200

    about.py aangemaakt en geimporteerd in init.py

--- webapp/secretsapp/__init__.py
+++ webapp/secretsapp/__init__.py
@@ -8,11 +8,13 @@ def create_app() -> Flask:
     app = Flask(__name__)
     from .views import views
     from .auth import auth
+    from .about import about
     app.config["SECRET_KEY"] = secrets.token_hex(64)
 
     app.teardown_appcontext(db.teardown_db)
 
     app.register_blueprint(secretsapp.home.bp)
-    app.register_blueprint(views, url_prefix="/views/")
-    app.register_blueprint(auth, url_prefix="/auth/" )
+    app.register_blueprint(views, url_prefix="/sign-up/")
+    app.register_blueprint(auth, url_prefix="/login/" )
+    app.register_blueprint(about, url_prefix="/about/")
     return app
