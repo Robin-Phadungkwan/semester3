4f01304db8b8ba632afcf843499c0e78204311b5
Robin Phadungkwan <robin.phadungkwan@hva.nl>
2023-09-15 20:13:41 +0200

    alle flask locaties staan hier

--- webapp/secretsapp/home.py
+++ webapp/secretsapp/home.py
@@ -1,6 +1,21 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 bp = Blueprint("home", __name__)
+about = Blueprint("about",__name__)
+views = Blueprint("views",__name__)
+auth = Blueprint("auth",__name__)
+
+@auth.route('/login/',methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        pass
+    return render_template("login.html")
+@views.route('/register/',methods=['GET','POST'])
+def signup():
+    return render_template("sign-up.html")
+@about.route('/')
+def over():
+    return render_template("about.html")
 
 @bp.route("/")
 def index():
