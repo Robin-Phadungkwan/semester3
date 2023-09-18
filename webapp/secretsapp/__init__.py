cd6f2c8a1fe837f2f9e03c996281a88c2ddec732
robin phadungkwan <robin.phadungkwan@hva.nl>
2023-09-18 23:17:43 +0200

    sync

--- webapp/secretsapp/__init__.py
+++ webapp/secretsapp/__init__.py
@@ -6,7 +6,7 @@ import secretsapp.db as db
 #hieronder wordt de flask app gemaakt en van .home de views, auth en about geimporteerd.
 def create_app() -> Flask:
     app = Flask(__name__)
-    from .home import views,auth,about
+    from .home import views,auth,about,loggedin
     app.config["SECRET_KEY"] = secrets.token_hex(64)
 
     app.teardown_appcontext(db.teardown_db)
@@ -15,4 +15,5 @@ def create_app() -> Flask:
     app.register_blueprint(views, url_prefix="/sign-up/")
     app.register_blueprint(auth, url_prefix="/login/" )
     app.register_blueprint(about, url_prefix="/about/")
+    app.register_blueprint(loggedin, url_prefix="/logged-in/")
     return app
