371352db47ef1c28a2c943ce9244f264214e3074
robin phadungkwan <robin.phadungkwan@hva.nl>
2023-09-20 00:15:47 +0200

    dingen uitproberen

--- webapp/secretsapp/home.py
+++ webapp/secretsapp/home.py
@@ -2,7 +2,10 @@
 import functools
 from flask import Flask, Blueprint,render_template, request,session,url_for,redirect
 from werkzeug.security import check_password_hash, generate_password_hash
+#from flask_mysqldb import MySQL
+import mysql.connector
 
+app = Flask(__name__)
 #hier worden de blueprints gemaakt.
 bp = Blueprint("home", __name__)
 about = Blueprint("about",__name__)
@@ -10,11 +13,34 @@ views = Blueprint("views",__name__)
 auth = Blueprint("auth",__name__)
 userlogged = Blueprint("userlogged",__name__)
 
-
+cnx = mysql.connector.connect(
+            host="secrets-db",
+            user="secrets",
+            password="BestPassword",
+            database="secrets"
+        )
+cursor = cnx.cursor()
+#app.config['MYSQL_HOST'] = 'localhost'
+#app.config['MYSQL_USER'] = 'secrets'
+#app.config['MYSQL_PASSWORD'] = 'BestPassword'
+#app.config['MYSQL_DB'] = 'secretsapp'
+#mysql = MySQL(app)*
 #hier maak ik de routes aan naar de website en geef ik login en sign-up een post/get methode mee
-
 @auth.route('/',methods=['GET','POST'])
 def login():
+    if request.method == 'POST':
+        add_user = ("INSERT INTO User" "(username, password_hash)" "VALUES (%s, %s)")
+        data_user = (request.form['username'], request.form['password'])
+        cursor.execute(add_user, data_user)
+        cnx.commit()
+        cursor.close()
+        cnx.close()
+
+        #cur = mysql.connection.cursor()
+        #cur.execute("INSERT INTO users(username,password_hash) VALUES(%s,%s)",(username,password))
+        #mysql.connection.commit()
+        #cur.close()
+        return redirect(url_for('auth.login'))
     return render_template("login.html")
 
 @views.route('/',methods=['GET','POST'])
