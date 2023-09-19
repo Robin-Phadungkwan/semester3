abf6b95763f5bf3c0e630dc84bba988d7af452a1
Robin Phadungkwan <robin.phadungkwan@hva.nl>
2023-09-19 21:02:47 +0200

    sync

--- webapp/secretsapp/__init__.py
+++ webapp/secretsapp/__init__.py
@@ -6,7 +6,7 @@ import secretsapp.db as db
 #hieronder wordt de flask app gemaakt en van .home de views, auth en about geimporteerd.
 def create_app() -> Flask:
     app = Flask(__name__)
-    from .home import views,auth,about,loggedin
+    from .home import views,auth,about,userlogged
     app.config["SECRET_KEY"] = secrets.token_hex(64)
 
     app.teardown_appcontext(db.teardown_db)
@@ -15,5 +15,5 @@ def create_app() -> Flask:
     app.register_blueprint(views, url_prefix="/register/")
     app.register_blueprint(auth, url_prefix="/login/")
     app.register_blueprint(about, url_prefix="/about/")
-    app.register_blueprint(loggedin, url_prefix="/logged-in/")
+    app.register_blueprint(userlogged, url_prefix="/logged-in/")
     return app
--- webapp/secretsapp/home.py
+++ webapp/secretsapp/home.py
@@ -1,90 +1,32 @@
 #hier wordt aangegeven wat er geimporteerd moet worden.
 import functools
-from flask import Blueprint,flash,g,redirect, render_template, request,session,url_for
+from flask import Flask, Blueprint,render_template, request,session,url_for,redirect
 from werkzeug.security import check_password_hash, generate_password_hash
-from db import get_db
-import mysql.connector
 
 #hier worden de blueprints gemaakt.
 bp = Blueprint("home", __name__)
 about = Blueprint("about",__name__)
 views = Blueprint("views",__name__)
 auth = Blueprint("auth",__name__)
-loggedin= Blueprint("logged-in",__name__)
-def db_connection():
-    con
+userlogged = Blueprint("userlogged",__name__)
+
 
 #hier maak ik de routes aan naar de website en geef ik login en sign-up een post/get methode mee
 
 @auth.route('/',methods=['GET','POST'])
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        db = get_db()
-        error = None
-        user = db.execute(
-            "SELECT * FROM user WHERE username = ?",(username,)
-        ).fetchone()
-
-        if user is None:
-            error = 'incorrect Username'
-        elif not check_password_hash(user['password'],password):
-            error = 'incorrect Password'
-
-        if error is None:
-            session.clear()
-            session['user_id'] = user['id']
-            return redirect(url_for("logged-in"))
-        flash(error)
     return render_template("login.html")
 
 @views.route('/',methods=['GET','POST'])
-def signup():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        db = get_db()
-        error = None
-        if not username:
-            error = 'username is required'
-        elif not password:
-            error = 'password is required'
-
-        if error is None:
-            try:
-                db.execute(
-                    "INSERT INTO user (username,password) VALUES (?,?)",
-                    (username,generate_password_hash(password))
-                )
-                db.commit()
-            except db.IntegrityError:
-                error = f"user {username} is already registered"
-            else:
-                return redirect(url_for("auth.login"))
-        flash(error)
+def signup():  
     return render_template("sign-up.html")
-@bp.before_app_request
-def load_logged_in_user():
-    user_id = session.get('user_id')
 
-    if user_id is None:
-        g.user = None
-    else:
-        g.user = get_db().execute(
-            'SELECT * FROM user WHERE id = ?', (user_id,)
-        ).fetchone()
-
-@bp.route('/logout')
-def logout():
-    session.clear()
-    return redirect(url_for('index'))
 
 @about.route('/')
 def over():
     return render_template("about.html")
 
-@loggedin.route('/')
+@userlogged.route('/')
 def loggedin():
     return render_template("logged-in.html")
 
