d0eaf36cde1ad65908c18d3926af2c51ff04b2c0
robin phadungkwan <robin.phadungkwan@hva.nl>
2023-09-21 21:00:21 +0200

    sync

--- webapp/secretsapp/home.py
+++ webapp/secretsapp/home.py
@@ -2,8 +2,7 @@
 import functools
 from flask import Flask, Blueprint,render_template, request,session,url_for,redirect
 from werkzeug.security import check_password_hash, generate_password_hash
-#from flask_mysqldb import MySQL
-import mysql.connector
+
 
 app = Flask(__name__)
 #hier worden de blueprints gemaakt.
@@ -13,34 +12,15 @@ views = Blueprint("views",__name__)
 auth = Blueprint("auth",__name__)
 userlogged = Blueprint("userlogged",__name__)
 
-cnx = mysql.connector.connect(
-            host="secrets-db",
-            user="secrets",
-            password="BestPassword",
-            database="secrets"
-        )
-cursor = cnx.cursor()
-#app.config['MYSQL_HOST'] = 'localhost'
-#app.config['MYSQL_USER'] = 'secrets'
-#app.config['MYSQL_PASSWORD'] = 'BestPassword'
-#app.config['MYSQL_DB'] = 'secretsapp'
-#mysql = MySQL(app)*
-#hier maak ik de routes aan naar de website en geef ik login en sign-up een post/get methode mee
+
 @auth.route('/',methods=['GET','POST'])
 def login():
     if request.method == 'POST':
-        add_user = ("INSERT INTO User" "(username, password_hash)" "VALUES (%s, %s)")
-        data_user = (request.form['username'], request.form['password'])
-        cursor.execute(add_user, data_user)
-        cnx.commit()
-        cursor.close()
-        cnx.close()
-
-        #cur = mysql.connection.cursor()
-        #cur.execute("INSERT INTO users(username,password_hash) VALUES(%s,%s)",(username,password))
-        #mysql.connection.commit()
-        #cur.close()
-        return redirect(url_for('auth.login'))
+        username = request.form['username']
+        password = request.form['password']
+        return redirect(url_for('userlogged.loggedin', username = username, password = password))
+    else:
+        pass
     return render_template("login.html")
 
 @views.route('/',methods=['GET','POST'])
@@ -52,8 +32,11 @@ def signup():
 def over():
     return render_template("about.html")
 
-@userlogged.route('/')
+@userlogged.route('/', methods=['GET','POST'])
 def loggedin():
+    if request.method == 'POST':
+        secrets = request.form['secrets']
+        redirect
     return render_template("logged-in.html")
 
 @bp.route("/")
