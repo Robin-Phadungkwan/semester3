4404776b26f365a7f5b917d5800ada2fd9d6bb12
robin phadungkwan <robin.phadungkwan@hva.nl>
2023-10-01 21:43:02 +0200

    wat functions aangemaakt en bijgewerkt

--- webapp/secretsapp/db.py
+++ webapp/secretsapp/db.py
@@ -35,25 +35,34 @@ def insert_user(username, hashpw):
     cursor.close()
     db.commit()
 
-def select_user(username, password_hash):
+def select_user(username):
     db = db_connection()  # Assuming this function returns a database connection
     cursor = db.cursor()
-    sql = "SELECT username FROM User WHERE username = %s and password_hash = %s"
-    val = ( username, password_hash,)  # Use a tuple with a single element
+    sql = "SELECT username FROM User WHERE username = %s"
+    val = ( username,)  # Use a tuple with a single element
     cursor.execute(sql, val)
     result = cursor.fetchone()
     cursor.close()
+    db.commit()
     return result
 
+def select_password(username):
+    db = db_connection()  # Assuming this function returns a database connection
+    cursor = db.cursor()
+    sql = "SELECT password_hash FROM User WHERE username = %s"
+    val = ( username,)  # Use a tuple with a single element
+    cursor.execute(sql, val)
+    result = cursor.fetchone()
+    cursor.close()
+    db.commit()
+    return result
 
-
-
-def insert_secret(name,info, user_name):
+def insert_secret(name,info,username):
     db = db_connection()
     cursor = db.cursor()
-    sql = "INSERT INTO secrets.Secret (id,name,info,user_name) VALUES (%s,%s, %s, %s)"
-    val = (name,info,user_name)
-    result = cursor.executemany(sql, val)
+    sql = "INSERT INTO secrets.Secret (name,info,user_name) VALUES (%s, %s, %s)"
+    val = (name,info,username)
+    result = cursor.execute(sql, val)
     db.commit()
     print(result)
     print ("Record inserted successfully into secrets table")
@@ -61,11 +70,11 @@ def insert_secret(name,info, user_name):
     db.commit()
 
 
-def select_secret(name,info, username):
+def select_secret(username):
     db = db_connection()  # Assuming this function returns a database connection
     cursor = db.cursor()
-    sql = "SELECT name, info, user_name FROM secrets.Secret WHERE name = %s and info = %s and user_name = %s"
-    val = ( name,info,username)  # Use a tuple with a single element
+    sql = "SELECT * FROM secrets.Secret WHERE user_name = %s"
+    val = (username,)  # Use a tuple with a single element
     cursor.execute(sql, val)
     result = cursor.fetchone()
     cursor.close()
