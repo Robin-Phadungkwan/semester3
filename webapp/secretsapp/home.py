07490f99c23936f9c5f3a6e89232bcbff18dcc39
Robin Phadungkwan <robin.phadungkwan@hva.nl>
2023-10-02 20:39:30 +0200

    comments toegevoegd, deel 1

--- webapp/secretsapp/home.py
+++ webapp/secretsapp/home.py
@@ -8,11 +8,11 @@ from datetime import timedelta
 #hier worden de blueprints gemaakt.
 app = Flask(__name__)
 bp = Blueprint("home", __name__)
-app.permanent_session_lifetime = timedelta(minutes=5)
+app.permanent_session_lifetime = timedelta(minutes=5) #hier wordt de session lifetime gemaakt.
 # maakt dat de wachtwoorden worden gehashed.
 
 @property
-def password(self):
+def password(self): 
     raise AttributeError('password is not a readable attribute')
 
 @password.setter
@@ -32,12 +32,12 @@ def login():
         #hier wordt gekeken of de username en het wachtwoord kloppen aan die in de database, zoja dan moet er hij naar de loggedin pagina.
         #de check_password_hash is een functie die kijkt of het wachtwoord klopt aan de hash en kies de eerste die hij ziet.
         if user and check_password_hash(hashed[0], password):
-            session.permanent = True
-            session["username"] = username
-            flash("Login successful", "success")
-            return redirect(url_for('home.loggedin'))
+            session.permanent = True #hier wordt de session permanent gemaakt.
+            session["username"] = username #hier wordt de username in de session gezet.
+            flash("Login successful", "success") #hier wordt een flash op het scherm gezet.
+            return redirect(url_for('home.loggedin')) #hier wordt je gereturned naar de loggedin pagina.
         else:
-            flash("Invalid username or password", "error")
+            flash("Invalid username or password", "error") #hier wordt een flash op het scherm gezet als de login of password fout is.
 
     if 'username' in session:
         return redirect(url_for('home.loggedin'))
@@ -49,10 +49,11 @@ def login():
 def signup(): 
     #post request om de data aan te maken
     if request.method == 'POST':
-        error = None
+        #hier wordt de hash gemaakt van het wachtwoord.
         hashpw = generate_password_hash(request.form['password'], method= 'pbkdf2:sha256',salt_length=12)
         #hier wordt de username en het wachtwoord van de form afgenomen.
         Username = request.form['username']
+        #hier wordt gekeken of de username en het wachtwoord niet te lang zijn en als hij te lang is dan moet er een flash op het scherm komen.
         if len (Username) > 256:
             flash ('Username is too long')
         password = request.form['password']
@@ -60,12 +61,16 @@ def signup():
             flash ('Password is too long')
         #hier wordt de data in de database gestopt.
         passed = insert_user(Username, hashpw)
+        #als de data al bestaat in de database dan moet er een flash op het scherm komen waarin wordt gezegd dat de username al bestaat(db.py) en wordt je gereturned.
         if passed == None:
             return render_template("sign-up.html")
-        else:   
+        else:   #anders mag je naar de login pagina.
             flash ('You are now registered')
             return redirect (url_for('home.login'))
-    return render_template("sign-up.html")
+    if 'username' in session:
+        return redirect(url_for('home.loggedin'))
+        
+    return render_template("sign-up.html") #hier wordt de sign-up pagina gerendered.
 
 
 @bp.route('/about/')
@@ -74,24 +79,24 @@ def over():
 
 @bp.route('/logged-in/', methods=['GET','POST'])
 def loggedin():
-    if 'username' in session:
-        Username = session['username']
-        if request.method == 'POST':
+    if 'username' in session:# als de username in de session zit dan mag je naar de logged-in pagina.
+        Username = session['username'] #hier wordt de username uit de session gehaald.
+        if request.method == 'POST': #als de request een post is dan mag je de data in de database stoppen.
             name = request.form['name']
             info = request.form['info']
             Username = session['username']
-            insert_secret(name,info,Username)
-            flash ('You have added a secret')
-            return redirect (url_for('home.loggedin', flash=flash, Username=Username, insert_secret=insert_secret))
-        return render_template("logged-in.html",flash=flash,insert_secret=insert_secret, Username=Username)
-    else:
+            insert_secret(name,info,Username) #hier wordt de data in de database gestopt.
+            flash ('You have added a secret') #hier wordt een flash op het scherm gezet.
+            return redirect (url_for('home.loggedin', flash=flash, Username=Username, insert_secret=insert_secret)) #hier wordt je gereturned naar de logged-in pagina met wat data.
+        return render_template("logged-in.html",flash=flash,insert_secret=insert_secret, Username=Username) #hier wordt de logged-in pagina gerendered met username en flash en secret data.
+    else: #als de username niet in de session zit dan wordt je gereturned naar de login pagina.
         return redirect(url_for('home.login'))
     
-@bp.route("/logout")
-def logout():
-    session.pop("username", None)
-    flash("You have been logged out.","info")
-    return redirect(url_for("home.index"))
+@bp.route("/logout") #hier wordt de logout functie gemaakt.
+def logout(): #hier wordt de logout functie gemaakt.
+    session.pop("username", None) #hier wordt de username uit de session gehaald.
+    flash("You have been logged out.","info") #hier wordt een flash op het scherm gezet.
+    return redirect(url_for("home.index")) #hier wordt je gereturned naar de index pagina.
 
 @bp.route("/")
 def index():
