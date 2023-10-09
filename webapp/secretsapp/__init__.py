8204c349506d0294cef300398b1da17ebb1068ad
robin phadungkwan <robin.phadungkwan@hva.nl>
2023-10-09 22:30:44 +0200

    witregels weggehaald

--- webapp/secretsapp/__init__.py
+++ webapp/secretsapp/__init__.py
@@ -6,10 +6,7 @@ import secretsapp.db as db
 #hieronder wordt de flask app gemaakt en van .home de views, auth en about geimporteerd.
 def create_app() -> Flask:
     app = Flask(__name__)
-    
     app.config["SECRET_KEY"] = secrets.token_hex(64)
-    
-
     app.teardown_appcontext(db.teardown_db)
     #hieronder wordt de blueprint geregistreerd.
     app.register_blueprint(secretsapp.home.bp)
